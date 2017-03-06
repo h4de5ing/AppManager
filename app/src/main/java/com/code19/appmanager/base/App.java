0bc9f77e44147c8fe3bcc80228107afe6d131029
h4de5ing <lex109@qq.com>
2017-03-06 23:42:31 +0800

    add bugly

--- app/src/main/java/com/code19/appmanager/base/App.java
+++ app/src/main/java/com/code19/appmanager/base/App.java
@@ -2,6 +2,8 @@ package com.code19.appmanager.base;
 
 import android.app.Application;
 
+import com.tencent.bugly.crashreport.CrashReport;
+
 
 /**
  * Created by gh0st on 2017/2/18.
@@ -14,6 +16,7 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         sApp = this;
+        CrashReport.initCrashReport(getApplicationContext(), "7c33bdd2-8fa5-4f18-b974-b6bba0d6f42e", false);
     }
 
     @Override
